cf46a24dbf987cda355baa244ce7d670f0b51223
Michael <mcg2570@rit.edu>
2019-10-16 12:13:59 -0400

    Added a Post() call

---
+++ mgServices/src/main/java/main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class main{
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        get("https://pokeapi.co/api/v2/pokemon/ditto/");
+    }
+
+
+}
